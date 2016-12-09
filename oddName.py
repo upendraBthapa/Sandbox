439758a6b1c7ad8f510e473d84691840e09452e3
jc449735 <upendrabthapa@my.jcu.edu.au>
2016-12-09 20:02:00 +1000

    complete file

--- oddName.py
+++ oddName.py
@@ -2,3 +2,12 @@ __author__ = 'jc449735'
 """
  upendra b thapa
 """
+valid = False
+name=input("enter your name")
+L=len(name)
+while not valid:
+  if name != "" and not name.isspace():
+    valid = True
+
+for l in range(0,L,2):
+    print(name[l])
